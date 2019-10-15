871b4539e0340bb2b824f2cf61a951cb570c243d
dheeraj <dheeraj.com>
2019-10-15 23:34:53 +0530

    django installed successfully, server running well

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7&(=k648s&*0n25%rcs=71)=jt8pte-#9@x!i35u372hv&2&!f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.7', 'localhost','127.0.0.1']
 
 
 # Application definition
