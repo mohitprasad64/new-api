d7935e73f76aef23d1d30bcc1bdd8483ff7a6bd8
dheeraj <dheeraj.com>
2019-10-26 22:59:32 +0530

    created profiles api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,7 +3,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+
+from profiles_api import permissions
 from profiles_api import serializers
+from profiles_api import models
+
 
 class HelloApiView(APIView):
 
@@ -179,3 +184,15 @@ class HelloViewSet(viewsets.ViewSet):
 
 
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+
+    """Handle creating, creating and updating profiles"""
+
+    serializer_class = serializers.UserProfileSerializer
+
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+
+    permission_classes = (permissions.UpdateOwnProfile,)
