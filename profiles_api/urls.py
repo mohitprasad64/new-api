be8508de39fbda40634445fa8eb5923b86494197
dheeraj <dheeraj.com>
2019-10-27 13:51:12 +0530

    feed added with permission to change only by user

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,6 +7,7 @@ router = DefaultRouter()
 
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profiles',views.UserProfileViewSet)
+router.register('feed',views.UserProfileFeedViewSet)
 urlpatterns = [
 
     path('hello-view/', views.HelloApiView.as_view()),
