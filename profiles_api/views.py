4cda00bf61666c84eb71847732c8c315aeb114f4
dheeraj <dheeraj.com>
2019-10-28 23:02:24 +0530

    only authenticated user can see the feed

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,7 +6,7 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from rest_framework.settings import api_settings
 
@@ -228,7 +228,7 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
 
         permissions.UpdateOwnStatus,
 
-        IsAuthenticatedOrReadOnly
+        IsAuthenticated
         )
 
 
